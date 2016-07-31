b3020bdb0538118dbbc9d8eaa9d9c619d557bb53
Andre Young <Dreyoung79@yahoo.com>
2016-07-31 13:14:12 -0400

    Working subcomponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-
+import { CustomerComponent } from './customer/customer.component';
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/app.component.html'         
+    templateUrl: 'app/app.component.html',
+    directives:[CustomerComponent]         
 })
 
 export class AppComponent { 
     title = 'Customer App';
     name = "Andre";
     nameColor = 'red';
+    customers = [
+      {id:1, name:"Andre"},  
+      {id:1, name:"India"},  
+      {id:1, name:"Annuetta"},  
+      {id:1, name:"Alyssa"},  
+    ];
 
     changeColor = function(){
         if(this.nameColor === 'red')
