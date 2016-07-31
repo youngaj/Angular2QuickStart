bd219ab34426ae6d45fb94da0153054162ca9390
Andre Young <Dreyoung79@yahoo.com>
2016-07-31 16:57:00 -0400

    Customers using Observable converted to Promise

--- app/customer/customer.service.ts
+++ app/customer/customer.service.ts
@@ -12,6 +12,16 @@ export class CustomerService {
     getCustomers(){
         return this._http.get(URL_CUSTOMER)
             .map((response: Response) => response.json())
+            .toPromise()
+            .catch((err) => { 
+                console.log(err);
+                return Promise.reject(err);
+            });
+    }
+
+    getCustomers_Observable(){
+        return this._http.get(URL_CUSTOMER)
+            .map((response: Response) => response.json())
             .catch(this._handleError);
     }
 
--- app/customer/customers.component.ts
+++ app/customer/customers.component.ts
@@ -14,15 +14,20 @@ export class CustomersComponent implements OnInit {
     constructor(private _customerService:CustomerService) {
 
      }
-    @Input() customers:Observable<any>;
+    @Input() customers:Promise<any>;
     @Input() customerColor:string;
 
     ngOnInit() { 
         this.customers = this._customerService.getCustomers()
                             .catch((err) => {
                                 console.log(err) //show pretty message to users
-                                return Observable.of((true)); //eats the error
                             });
+        
+        // this.customers = this._customerService.getCustomers()
+        //                     .catch((err) => {
+        //                         console.log(err) //show pretty message to users
+        //                         return Observable.of((true)); //eats the error
+        //                     });
     }
 
 }
