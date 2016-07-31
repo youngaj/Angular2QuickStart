2ac0f0085077556517898914e4973d1aff26dd53
Andre Young <Dreyoung79@yahoo.com>
2016-07-31 17:00:31 -0400

    Customers using promise without async
    Use [ ] whenever you need to manipulate the data before going to the UI.

--- app/customer/customers.component.ts
+++ app/customer/customers.component.ts
@@ -14,14 +14,20 @@ export class CustomersComponent implements OnInit {
     constructor(private _customerService:CustomerService) {
 
      }
-    @Input() customers:Promise<any>;
+    @Input() customers:any[];
     @Input() customerColor:string;
 
     ngOnInit() { 
-        this.customers = this._customerService.getCustomers()
-                            .catch((err) => {
+        this._customerService.getCustomers()
+                             .then((customerResponse) => { this.customers = customerResponse})
+                             .catch((err) => {
                                 console.log(err) //show pretty message to users
                             });
+
+        // this.customers = this._customerService.getCustomers()
+        //                     .catch((err) => {
+        //                         console.log(err) //show pretty message to users
+        //                     });
         
         // this.customers = this._customerService.getCustomers()
         //                     .catch((err) => {
